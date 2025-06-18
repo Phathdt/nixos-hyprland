75c761c245a996dc6dc90a8f180057874a7d7371
Phathdt <phathdt379@gmail.com>
2025-06-18 15:56:29 +0700

    Revert "add kb 2"
    This reverts commit 4030edcd58547c427562b614cfafe84ba5f4475a.

--- config/scripts/keyboard-layout-switch.sh
+++ config/scripts/keyboard-layout-switch.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-# Get current keyboard layout
-current_layout=$(hyprctl devices | grep -A 10 "Keyboard" | grep "active keymap" | head -1 | awk '{print $3}')
-
-if [ "$current_layout" = "English" ] || [ "$current_layout" = "us" ]; then
-    # Switch to Vietnamese
-    hyprctl switchxkblayout at-translated-set-2-keyboard 1
-    notify-send "Keyboard Layout" "Switched to Vietnamese" -t 1000
-else
-    # Switch to English
-    hyprctl switchxkblayout at-translated-set-2-keyboard 0
-    notify-send "Keyboard Layout" "Switched to English" -t 1000
-fi
+ 
