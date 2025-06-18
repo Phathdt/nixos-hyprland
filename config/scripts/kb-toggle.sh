5438cd0a63449a174d3d1bbbfb84e72420565961
Phathdt <phathdt379@gmail.com>
2025-06-18 15:56:24 +0700

    Revert "add kb 4"
    This reverts commit 361e745b8a63c95a046930d7b4f45b5a582478cb.

--- config/scripts/kb-toggle.sh
+++ config/scripts/kb-toggle.sh
@@ -1,19 +1,8 @@
 #!/usr/bin/env bash
 
-# Auto-detect keyboard device name
-KEYBOARD_DEVICE=$(hyprctl devices | grep -B 1 "Keyboard" | grep -v "Keyboard" | grep -v "^--$" | head -1 | xargs)
-
-if [ -z "$KEYBOARD_DEVICE" ]; then
-    notify-send "Error" "No keyboard device found" -t 2000
-    echo "Error: No keyboard device found"
-    exit 1
-fi
-
-echo "Using keyboard device: $KEYBOARD_DEVICE"
-
 # Simple keyboard layout toggle
-hyprctl switchxkblayout "$KEYBOARD_DEVICE" next
+hyprctl switchxkblayout at-translated-set-2-keyboard next
 
 # Show notification
-layout=$(hyprctl devices | grep -A 10 "Keyboard" | grep "active keymap" | head -1 | awk '{print $3}')
+layout=$(hyprctl devices | grep -A 5 "at-translated-set-2-keyboard" | grep "active keymap" | awk '{print $3}')
 notify-send "Layout" "Current: $layout" -t 1000
