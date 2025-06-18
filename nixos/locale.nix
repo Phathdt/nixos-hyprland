37ddc50877a438721debe827fb2de4e918633cfa
Phathdt <phathdt379@gmail.com>
2025-06-18 15:41:01 +0700

    add ibus

--- nixos/locale.nix
+++ nixos/locale.nix
@@ -16,4 +16,20 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
+
+  # Try IBus Unikey as alternative
+  i18n.inputMethod = {
+    enable = true;
+    type = "ibus";
+    ibus.engines = with pkgs.ibus-engines; [
+      unikey
+      bamboo
+    ];
+  };
+
+  environment.variables = {
+    GTK_IM_MODULE = "ibus";
+    QT_IM_MODULE = "ibus";
+    XMODIFIERS = "@im=ibus";
+  };
 }
