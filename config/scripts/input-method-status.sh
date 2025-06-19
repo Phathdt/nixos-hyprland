ed7d4106f25b7ce07749374b76212107978012ee
Phathdt <phathdt379@gmail.com>
2025-06-19 10:08:44 +0700

    Revert "add ibus 4"
    This reverts commit 6d26b5a6ffcdabc7e20c7340d2db115769220b3b.

--- config/scripts/input-method-status.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CURRENT_ENGINE=$(ibus engine 2>/dev/null)
-
-if [[ "$CURRENT_ENGINE" == "Bamboo" ]]; then
-    echo '{"text": "🇻🇳", "tooltip": "Vietnamese (Bamboo)", "class": "vietnamese"}'
-else
-    echo '{"text": "🇺🇸", "tooltip": "English", "class": "english"}'
-fi
