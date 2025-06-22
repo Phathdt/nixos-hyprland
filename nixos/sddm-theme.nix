342c2626b3cd5f7686ed5f06f2d8104645724fdc
Phathdt <phathdt379@gmail.com>
2025-06-23 03:20:02 +0700

    add theme sddm 11

--- nixos/sddm-theme.nix
+++ nixos/sddm-theme.nix
@@ -4,21 +4,6 @@
   environment.systemPackages = with pkgs; [
     (pkgs.callPackage ./sddm-astronaut-theme.nix {
       theme = "black_hole";
-      themeConfig = {
-        General = {
-          FontSize = "12.0";
-          ScreenWidth = "1920";
-          ScreenHeight = "1080";
-        };
-        Design = {
-          MainColor = "#c792ea";
-          AccentColor = "#82aaff";
-          BackgroundColor = "#292d3e";
-          OverrideLoginButtonTextColor = "#eeffff";
-          Font = "JetBrainsMono Nerd Font";
-        };
-      };
     })
-    sddm-sugar-dark  # Backup theme để test
   ];
 }
