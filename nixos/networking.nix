464d29981c3a151f2a192d18177949c6c3d0530a
Phathdt <phathdt379@gmail.com>
2025-06-23 11:15:13 +0700

    update network

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -2,5 +2,19 @@
 
 {
   networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+
+  # DNS configuration for better VPN compatibility
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" "8.8.4.4" ];
+  networking.dhcpcd.extraConfig = "nohook resolv.conf";
+
+  # Enable systemd-resolved for better DNS handling
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
+    extraConfig = ''
+      DNSOverTLS=yes
+    '';
+  };
 }
--- nixos/services.nix
+++ nixos/services.nix
@@ -66,16 +66,24 @@
     enable = true;
     useRoutingFeatures = "client";
     # Auto-connect on boot (requires auth key setup)
-    extraUpFlags = [ "--accept-routes" ];
+    extraUpFlags = [ "--accept-routes" "--accept-dns" ];
   };
 
-  # NetworkManager with VPN support
+  # NetworkManager with VPN support - moved to networking.nix to avoid conflicts
   networking.networkmanager = {
     enable = true;
-    plugins = with pkgs; [ networkmanager-openvpn ];
+    plugins = with pkgs; [
+      networkmanager-openvpn
+      networkmanager-vpnc
+      networkmanager-l2tp
+    ];
+    # Don't override DNS when using VPN
+    dns = "systemd-resolved";
+    # Enable connection sharing
+    unmanaged = [ "docker0" "virbr0" ];
   };
 
-  # Disable IPv6 completely
+    # Disable IPv6 completely for VPN compatibility
   networking.enableIPv6 = false;
 
   # Additional IPv6 disable via kernel parameters
