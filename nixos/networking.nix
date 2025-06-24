016261411e062ea28dfacf2ad4aebbbda979fc60
Phathdt <phathdt379@gmail.com>
2025-06-24 22:25:06 +0700

    remove bind homelab

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -3,17 +3,6 @@
 {
   networking.hostName = "nixos";
 
-  # Homelab hosts
-  networking.hosts = {
-    "127.0.0.1" = [
-      "homelab.local"
-      "traefik.homelab.local"
-      "immich.homelab.local"
-      "portainer.homelab.local"
-      "netdata.homelab.local"
-    ];
-  };
-
   # DNS configuration for better VPN compatibility
   networking.nameservers = [ "1.1.1.1" "8.8.8.8" "8.8.4.4" ];
   networking.dhcpcd.extraConfig = "nohook resolv.conf";
